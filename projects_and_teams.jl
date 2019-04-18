cf1c3ca1ab6fa355f7a0da7c5fcd348808689c40
Milos Subotic <milos.subotic.sm@gmail.com>
2019-04-18 14:51:23 +0200

    Some project addded.

--- projects_and_teams.jl
+++ projects_and_teams.jl
@@ -1,86 +1,494 @@
 #!/usr/bin/env julia
 ###############################################################################
 
-type Student
-	name::String
-	surname::String
-	index::String
-	email::String
-	points::Int
-end
+include("Gen_Struct_and_Constructor.jl")
 
-type Team
-	members::Vector{Student}
-end
+
+gen_struct_and_constructor_with_must_specify(
+	:Student,
+	false,
+
+	:name,       :String,
+	:surname,    :String,
+	:index,      :String,
+	:email,      :String,
+	:points,     :Int,
+)
+
+gen_struct_and_constructor_with_must_specify(
+	:Team,
+	false,
+
+	:pre_registration, :String,
+	:members,          :(Vector{Student}),
+)
 
 const URL = String
-type Spec
-	text::String
-	resources::Vector{URL}
-end
 
-type Solution
-	repo::URL
+gen_struct_and_constructor_with_must_specify(
+	:Spec,
+	false,
+	
+	:sequel,         :Bool,
+	:have_support,   :Bool,
+	:text,           :String,
+	:resources,      :(Vector{URL}),
+)
+
+gen_struct_and_constructor_with_must_specify(
+	:Solution,
+	false,
+	
+	:repo,           :URL,
 	# Short youtube presentation.
-	youtube::URL
+	:youtube,        :URL,
 	# download.bit HW+SW
 	# Could set URL to file in github master blob.
-	release_bit::Union{URL,Vector{URL}}
+	:release_bit,    :(Union{URL,Vector{URL}}),
 	# Short project documentation.
 	# Could set URL to file in github master blob.
-	doc::URL
-end
+	:doc,            :URL,
+)
 
-type Project
-	name::String
-	points::Int
-	min_team_members::Int
-	team::Team
-	spec::Spec
-	solution::Solution
-end
-
+gen_struct_and_constructor_with_must_specify(
+	:Project,
+	false,
+	
+	:name,              :String,
+	:points,            :Int,
+	:min_team_members,  :Int,
+	:team,              :Team,
+	:spec,              :Spec,
+	:solution,          :Solution,
+)
 
 ###############################################################################
 
 projects = Project[
 	Project(
-		"Risico",
-		45,
-		3,
-		Team(
-			Student[
+		name             = "Legend Of Zelda",
+		points           = 45,
+		min_team_members = 3,
+		team = Team(
+			pre_registration = "",
+			members = Student[
 				Student(
-					"Name",
-					"Lastname",
-					"RA-XXX/20YY",
-					"xxx@yyy",
-					0
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
 				),
 				Student(
-					"Name",
-					"Lastname",
-					"RA-XXX/20YY",
-					"xxx@yyy",
-					0
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
 				),
-			]
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+			],
 		),
-		Spec(
-			"""
+		spec = Spec(
+			sequel       = true,
+			have_support = true,
+			text = """
 			""",
-			URL[
-				URL("https://github.com/vukse95/super_mario"),
-			]
+			resources = URL[
+				URL("https://github.com/CubeLord/LPRS2-Zelda.git"),
+				URL("https://www.youtube.com/watch?v=0CTFHpnBlLo"),
+			],
 		),
-		Solution(
-			"https://github.com/X/Y", # Git repo
-			"", # Youtube
-			"", # Bit file
-			"" # Doc file
-		)
+		solution = Solution(
+			repo        = "",
+			youtube     = "",
+			release_bit = "",
+			doc         = "",
+		),
 	),
-]
+	Project(
+		name             = "Šah",
+		points           = 45,
+		min_team_members = 3,
+		team = Team(
+			pre_registration = "Nemanja, Mihajlo, Milan",
+			members = Student[
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+			],
+		),
+		spec = Spec(
+			sequel       = false,
+			have_support = false,
+			text = """
+			""",
+			resources = URL[
+				URL(""),
+				URL(""),
+			],
+		),
+		solution = Solution(
+			repo        = "",
+			youtube     = "",
+			release_bit = "",
+			doc         = "",
+		),
+	),
+	Project(
+		name             = "Racing Kings",
+		points           = 45,
+		min_team_members = 3,
+		team = Team(
+			pre_registration = "Miloš, Slobodan, Arsenije",
+			members = Student[
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+			],
+		),
+		spec = Spec(
+			sequel       = false,
+			have_support = false,
+			text = """
+			""",
+			resources = URL[
+				URL(""),
+				URL(""),
+			],
+		),
+		solution = Solution(
+			repo        = "",
+			youtube     = "",
+			release_bit = "",
+			doc         = "",
+		),
+	),
+	Project(
+		name             = "Advance Tetris",
+		points           = 45,
+		min_team_members = 3,
+		team = Team(
+			pre_registration = "Andrej, Jovana",
+			members = Student[
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+			],
+		),
+		spec = Spec(
+			sequel       = true,
+			have_support = false,
+			text = """
+			""",
+			resources = URL[
+				URL("https://github.com/Sandra2606996/MSAK_tetris"),
+				URL("https://github.com/boza94/pizeo_buzz"),
+			],
+		),
+		solution = Solution(
+			repo        = "",
+			youtube     = "",
+			release_bit = "",
+			doc         = "",
+		),
+	),
+	Project(
+		name             = "Bomberman",
+		points           = 45,
+		min_team_members = 3,
+		team = Team(
+			pre_registration = "Nemanja, Aleksa, Saša",
+			members = Student[
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+			],
+		),
+		spec = Spec(
+			sequel       = true,
+			have_support = false,
+			text = """
+			""",
+			resources = URL[
+				URL("https://github.com/KakucaNebojsa/Boomberman"),
+				URL(""),
+			],
+		),
+		solution = Solution(
+			repo        = "",
+			youtube     = "",
+			release_bit = "",
+			doc         = "",
+		),
+	),
+	Project(
+		name             = "World's Hardest Game",
+		points           = 45,
+		min_team_members = 3,
+		team = Team(
+			pre_registration = "Nikola, Miodrag, Laza",
+			members = Student[
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+			],
+		),
+		spec = Spec(
+			sequel       = true,
+			have_support = false,
+			text = """
+			""",
+			resources = URL[
+				URL("https://github.com/AleksaArsic/LPRS2-WorldsHardestGame"),
+				URL(""),
+			],
+		),
+		solution = Solution(
+			repo        = "",
+			youtube     = "",
+			release_bit = "",
+			doc         = "",
+		),
+	),
+	Project(
+		name             = "Heads foodball",
+		points           = 45,
+		min_team_members = 3,
+		team = Team(
+			pre_registration = "Petar, Tamara, Miloš, Radovan",
+			members = Student[
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+			],
+		),
+		spec = Spec(
+			sequel       = false,
+			have_support = false,
+			text = """
+			""",
+			resources = URL[
+				URL(""),
+				URL(""),
+			],
+		),
+		solution = Solution(
+			repo        = "",
+			youtube     = "",
+			release_bit = "",
+			doc         = "",
+		),
+	),
+	Project(
+		name             = "Potapanje brodića",
+		points           = 45,
+		min_team_members = 3,
+		team = Team(
+			pre_registration = "Uroš, Vladimir, Srđan",
+			members = Student[
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+			],
+		),
+		spec = Spec(
+			sequel       = true,
+			have_support = false,
+			text = """
+			""",
+			resources = URL[
+				URL("https://github.com/uros96/potapanjeBrodica"),
+				URL(""),
+			],
+		),
+		solution = Solution(
+			repo        = "",
+			youtube     = "",
+			release_bit = "",
+			doc         = "",
+		),
+	),
+	Project(
+		name             = "Donkey Kong",
+		points           = 45,
+		min_team_members = 3,
+		team = Team(
+			pre_registration = "Nikolina, Mihajlo, Luka",
+			members = Student[
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+				Student(
+					name    = "",
+					surname = "",
+					index   = "RA-xxx/20yy",
+					email   = "@",
+					points  = 0,
+				),
+			],
+		),
+		spec = Spec(
+			sequel       = false,
+			have_support = false,
+			text = """
+			""",
+			resources = URL[
+				URL(""),
+				URL(""),
+			],
+		),
+		solution = Solution(
+			repo        = "",
+			youtube     = "",
+			release_bit = "",
+			doc         = "",
+		),
+	),
+]	
 
 additional = URL[
 	# Making bit file.
@@ -92,6 +500,9 @@ additional = URL[
 ]
 
 ###############################################################################
+
+#=
+#TODO Sum all marks, so we could check if studends changed mark field.
 
 N_projects = length(projects)
 @show N_projects
@@ -113,7 +524,7 @@ end
 function project_done(project::Project)
 	s = project.solution
 	done = s.repo != "https://github.com/X/Y" && s.youtube != "" && s.doc != ""
-		#s.release_bit != ""
+		#s.release_bit != "" 
 	return done
 end
 
@@ -132,7 +543,7 @@ if false
 			push!(resources, r)
 		end
 	end
-
+	
 	for r in resources
 		println("Resource: ", r)
 	end
@@ -154,6 +565,7 @@ if false
 		end
 	end
 end
+=#
 
 println("End")
 
