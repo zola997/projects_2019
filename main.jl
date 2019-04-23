b6a4bddbbf1f0e755fc6c6930cd7b6d4d47bd1ff
Milos Subotic <milos.subotic.sm@gmail.com>
2019-04-23 09:29:25 +0200

    Spliting.

---
+++ main.jl
@@ -0,0 +1,169 @@
+#!/usr/bin/env julia
+###############################################################################
+
+include("Gen_Struct_and_Constructor.jl")
+
+
+gen_struct_and_constructor_with_must_specify(
+	:Student,
+	false,
+
+	:name,       :String,
+	:surname,    :String,
+	:index,      :String,
+	:email,      :String,
+	:points,     :Int,
+)
+
+gen_struct_and_constructor_with_must_specify(
+	:Team,
+	false,
+
+	:pre_registration, :String,
+	:members,          :(Vector{Student}),
+)
+
+const URL = String
+
+gen_struct_and_constructor_with_must_specify(
+	:Spec,
+	false,
+	
+	:sequel,         :Bool,
+	:have_support,   :Bool,
+	:text,           :String,
+	:resources,      :(Vector{URL}),
+)
+
+gen_struct_and_constructor_with_must_specify(
+	:Solution,
+	false,
+	
+	:repo,           :URL,
+	# Short youtube presentation.
+	:youtube,        :URL,
+	# download.bit HW+SW
+	# Could set URL to file in github master blob.
+	:release_bit,    :(Union{URL,Vector{URL}}),
+	# Short project documentation.
+	# Could set URL to file in github master blob.
+	:doc,            :URL,
+)
+
+gen_struct_and_constructor_with_must_specify(
+	:Project,
+	false,
+	
+	:name,              :String,
+	:points,            :Int,
+	:min_team_members,  :Int,
+	:team,              :Team,
+	:spec,              :Spec,
+	:solution,          :Solution,
+)
+
+###############################################################################
+
+include("projects_and_teams.jl")
+
+###############################################################################
+
+#TODO Sum all marks, so we could check if studends changed mark field.
+
+N_projects = length(projects)
+@show N_projects
+
+# Generating tex.
+if true
+	open("Projects.gen.tex", "w") do f
+		w(args...) = println(f, args...)
+		for (idx, p) in enumerate(projects[1:2])
+			w("\\subsection{Пројекат $idx}")
+			w("\\begin{frame}")
+			w("	\\frametitle{\\textbf{$(p.name)}}")
+			
+			w("	\\begin{itemize}")
+				i(s) = w("		\\item " * s)
+				if p.spec.sequel
+					if p.spec.have_support
+						h = " (уз помоћ старијих колега)"
+					else
+						h = ""
+					end
+					i("Наставак од прошле године" * h)
+				end
+				if p.team.pre_registration != ""
+					i("Заузето ($(p.team.pre_registration))")
+				end
+			w("	\\end{itemize}")
+			w("\\end{frame}")
+			w("")
+		end
+	end
+	run(`latexmk -silent -pdf LPRS2_projekti.tex`)
+end
+
+
+function no_of_students(project::Project)
+	if project.team.members[1].name == "Name"
+		return 0
+	else
+		return length(project.team.members)
+	end
+end
+N_students = sum(map(no_of_students, projects))
+@show N_students
+
+function project_taken(project::Project)
+	!any(map((member) -> member.name == "Name", project.team.members))
+end
+
+function project_done(project::Project)
+	s = project.solution
+	done = s.repo != "https://github.com/X/Y" && s.youtube != "" && s.doc != ""
+		#s.release_bit != "" 
+	return done
+end
+if false
+	for project in projects
+		if project_taken(project) && !project_done(project)
+			println("Not done: ", project.name)
+		end
+	end
+end
+
+#=
+if false
+	resources = Set{URL}()
+	for project in projects
+		for r in project.spec.resources
+			push!(resources, r)
+		end
+	end
+	
+	for r in resources
+		println("Resource: ", r)
+	end
+end
+
+N_youtube = 0
+for project in projects
+	if project_taken(project) && project.solution.youtube != ""
+		N_youtube += 1
+	end
+end
+@show N_youtube
+
+if false
+	println("Youtube URLs:")
+	for project in projects
+		if project_taken(project) && project.solution.youtube != ""
+			println(project.solution.youtube)
+		end
+	end
+end
+=#
+
+println("End")
+
+###############################################################################
